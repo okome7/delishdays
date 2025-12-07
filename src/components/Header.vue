52f21148171114238b6b5d06d20d22cfecefa5e1
okome7 <jiumijingyouyi@gmail.com>
2025-12-07 21:41:14 +0900

    add

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -274,6 +274,7 @@ const goBack = () => {
   background-color: #f5f5f5;
   cursor: pointer;
   transition: background-color 0.2s;
+  color: #333;
 
   -webkit-tap-highlight-color: transparent;
   -webkit-appearance: none;

eafda3cf1865365dd1698aaaabbe383cbfad3d57
okome7 <jiumijingyouyi@gmail.com>
2025-12-13 10:03:48 +0900

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
-  base: "./",
+  base: "/delishdays/",
   plugins: [vue(), svgLoader()],
 });

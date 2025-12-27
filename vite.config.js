58f3858fc36c8eefd8797ee3c1ddd19d631adb50
okome7 <jiumijingyouyi@gmail.com>
2025-12-27 14:51:09 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
-  base: "/delishdays/",
+  base: isProduction ? "/delishdays/" : "/",
   plugins: [vue(), svgLoader()],
 });
